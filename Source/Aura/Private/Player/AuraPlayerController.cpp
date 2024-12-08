1a07d9954618a1e1d88724e2a9756bf3054101a0
himanshu22-cse <70806091+himanshu22-cse@users.noreply.github.com>
2024-12-09 02:41:02 +0530

    /

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -279,6 +279,7 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	{
 		return;
 	}
+
 	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
 	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f); 
